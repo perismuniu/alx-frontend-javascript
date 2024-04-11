dba304a25b690beb6a96d43790d6dba75bcc9c49
perismuniu <peris.muniu@gmail.com>
2024-04-11 16:45:04 +0000

    5. A Building - correction

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,14 @@
 export default class Building {
-    constructor(sqft) {
-        this._sqft = 0;
-        this.sqft = sqft;
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
-    get sqft() {
-        return this._sqft;
-    }
+    this._sqft = sqft;
+  }
 
-    set sqft(value) {
-        if (typeof value !== "number") {
-            throw new TypeError("Sqft must be a number");
-        }
-        this._sqft = value;
-    }
-
-    evacuationWarningMessage() {
-        throw new Error("Class extending Building must override evacuationWarningMessage");
-    }
+  get sqft() {
+    return this._sqft;
+  }
 }

e3823473fc143c3b06c47eb0a7fb16c61108586f
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-04-16 21:19:37 +0300

    8. Clean set

---
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -0,0 +1,4 @@
+import cleanSet from "../0x00-ES6_basic/8-clean_set.js";
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
